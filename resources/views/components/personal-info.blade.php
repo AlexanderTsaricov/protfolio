3104db1d7f3c6b81d42b024afdc93cf771cbee60
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-06-10 18:24:33 +0500

    update styles in personal-info

--- resources/views/components/personal-info.blade.php
+++ resources/views/components/personal-info.blade.php
@@ -3,15 +3,65 @@
         <summary class="personalInfoBlock_summaryHead">personal-info</summary>
         <div class="personalInfoBlock_headBox">
             <details class="personalInfoBlock_details">
-                <summary class="personalInfoBlock_summary">interests</summary>
+                <summary class="personalInfoBlock_summary">
+                    <span class="personalInfoBlock_summary__span">
+                        <svg class="personalInfoBlock_summary__svg" id="svgIntrests" width="14" height="12"
+                            viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.6666 3.33333V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V2.66667H12.9999C13.3681 2.66667 13.6666 2.96515 13.6666 3.33333ZM7.27605 1.33333H0.333252V0.666667C0.333252 0.29848 0.631732 0 0.999919 0H5.94272L7.27605 1.33333Z" />
+                        </svg>
+                        interests
+                    </span>
+                </summary>
             </details>
             <details class="personalInfoBlock_details">
-                <summary class="personalInfoBlock_summary" id="educationButton">education</summary>
+                <summary class="personalInfoBlock_summary" id="educationButton">
+                    <span class="personalInfoBlock_summary__span">
+                        <svg class="personalInfoBlock_summary__svg" id="svgEducation" width="14" height="12"
+                            viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.6666 3.33333V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V2.66667H12.9999C13.3681 2.66667 13.6666 2.96515 13.6666 3.33333ZM7.27605 1.33333H0.333252V0.666667C0.333252 0.29848 0.631732 0 0.999919 0H5.94272L7.27605 1.33333Z" />
+                        </svg>
+                        education
+                    </span>
+                </summary>
                 <div class="personalInfoBlock_smallBox">
-                    <p class="personalInfoBlock_summary" id="schoolButton">School</p>
-                    <p class="personalInfoBlock_summary" id="schoolCollege">College</p>
-                    <p class="personalInfoBlock_summary" id="schoolUniversity">Univerity</p>
-                    <p class="personalInfoBlock_summary" id="schoolCourses">Courses</p>
+                    <p class="personalInfoBlock_summary" id="schoolButton">
+                        <svg width="14" height="12" viewBox="0 0 14 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M0.999919 0H12.9999C13.3681 0 13.6666 0.29848 13.6666 0.666667V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V0.666667C0.333252 0.29848 0.631732 0 0.999919 0ZM3.66659 8.33333V5.66667L4.99992 7L6.33325 5.66667V8.33333H7.66659V3.66667H6.33325L4.99992 5L3.66659 3.66667H2.33325V8.33333H3.66659ZM10.9999 6.33333V3.66667H9.66659V6.33333H8.33325L10.3333 8.33333L12.3333 6.33333H10.9999Z"
+                                fill="#62748E" />
+                        </svg>
+                        School
+                    </p>
+                    <p class="personalInfoBlock_summary" id="schoolCollege">
+                        <svg width="14" height="12" viewBox="0 0 14 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M0.999919 0H12.9999C13.3681 0 13.6666 0.29848 13.6666 0.666667V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V0.666667C0.333252 0.29848 0.631732 0 0.999919 0ZM3.66659 8.33333V5.66667L4.99992 7L6.33325 5.66667V8.33333H7.66659V3.66667H6.33325L4.99992 5L3.66659 3.66667H2.33325V8.33333H3.66659ZM10.9999 6.33333V3.66667H9.66659V6.33333H8.33325L10.3333 8.33333L12.3333 6.33333H10.9999Z"
+                                fill="#62748E" />
+                        </svg>
+                        College
+                    </p>
+                    <p class="personalInfoBlock_summary" id="schoolUniversity">
+                        <svg width="14" height="12" viewBox="0 0 14 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M0.999919 0H12.9999C13.3681 0 13.6666 0.29848 13.6666 0.666667V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V0.666667C0.333252 0.29848 0.631732 0 0.999919 0ZM3.66659 8.33333V5.66667L4.99992 7L6.33325 5.66667V8.33333H7.66659V3.66667H6.33325L4.99992 5L3.66659 3.66667H2.33325V8.33333H3.66659ZM10.9999 6.33333V3.66667H9.66659V6.33333H8.33325L10.3333 8.33333L12.3333 6.33333H10.9999Z"
+                                fill="#62748E" />
+                        </svg>
+                        Univerity
+                    </p>
+                    <p class="personalInfoBlock_summary" id="schoolCourses">
+                        <svg width="14" height="12" viewBox="0 0 14 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M0.999919 0H12.9999C13.3681 0 13.6666 0.29848 13.6666 0.666667V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V0.666667C0.333252 0.29848 0.631732 0 0.999919 0ZM3.66659 8.33333V5.66667L4.99992 7L6.33325 5.66667V8.33333H7.66659V3.66667H6.33325L4.99992 5L3.66659 3.66667H2.33325V8.33333H3.66659ZM10.9999 6.33333V3.66667H9.66659V6.33333H8.33325L10.3333 8.33333L12.3333 6.33333H10.9999Z"
+                                fill="#62748E" />
+                        </svg>
+                        Courses
+                    </p>
                 </div>
             </details>
         </div>
@@ -34,7 +84,7 @@
                     <path
                         d="M12 9.44667V11.804C12.0001 11.9728 11.9361 12.1353 11.8211 12.2588C11.706 12.3823 11.5484 12.4575 11.38 12.4693C11.0887 12.4893 10.8507 12.5 10.6667 12.5C4.77533 12.5 0 7.72467 0 1.83333C0 1.64933 0.01 1.41133 0.0306667 1.12C0.0424816 0.951629 0.117721 0.794007 0.241202 0.678939C0.364684 0.563871 0.527215 0.499924 0.696 0.5H3.05333C3.13603 0.499916 3.2158 0.530573 3.27715 0.586014C3.33851 0.641455 3.37706 0.717722 3.38533 0.8C3.40067 0.953334 3.41467 1.07533 3.428 1.168C3.56049 2.09262 3.832 2.99189 4.23333 3.83533C4.29667 3.96867 4.25533 4.128 4.13533 4.21333L2.69667 5.24133C3.5763 7.29097 5.2097 8.92436 7.25933 9.804L8.286 8.368C8.32796 8.30933 8.38919 8.26725 8.459 8.2491C8.52881 8.23095 8.60277 8.23787 8.668 8.26867C9.51135 8.66924 10.4104 8.94008 11.3347 9.072C11.4273 9.08533 11.5493 9.1 11.7013 9.11467C11.7835 9.12309 11.8596 9.16171 11.9149 9.22305C11.9702 9.28439 12.0008 9.36408 12.0007 9.44667H12Z"
                         fill="#62748E" />
-                </svg> 
+                </svg>
                 +79126062987
             </p>
         </div>
