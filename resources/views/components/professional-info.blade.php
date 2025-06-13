818b070a411dded2c1a0b970cef829e6ae80e078
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-06-13 18:48:58 +0500

    trasfer and rename contacts block to another file and add styles to professionalInfoBlock

---
+++ resources/views/components/professional-info.blade.php
@@ -0,0 +1,49 @@
+<head>
+    <link rel="stylesheet" href="{{ asset('css/professional-info/style.css') }}">
+</head>
+
+<div class="professionalInfoBlock">
+    <details class="professionalInfoBlock_detailsHead">
+        <summary class="professionalInfoBlock_summaryHead">professional-info</summary>
+        <div class="professionalInfoBlock_headBox">
+            <div class="professionalInfoBlock_box">
+                <summary class="professionalInfoBlock_summary" id="frontendButton">
+                    <span class="professionalInfoBlock_summary__span">
+                        <svg class="professionalInfoBlock_summary__svg" id="svgIntrests" width="14" height="12"
+                            viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.6666 3.33333V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V2.66667H12.9999C13.3681 2.66667 13.6666 2.96515 13.6666 3.33333ZM7.27605 1.33333H0.333252V0.666667C0.333252 0.29848 0.631732 0 0.999919 0H5.94272L7.27605 1.33333Z" />
+                        </svg>
+                        Front-end
+                    </span>
+                </summary>
+            </div>
+            <details class="professionalInfoBlock_details">
+                <summary class="professionalInfoBlock_summary" id="backendButton">
+                    <span class="professionalInfoBlock_summary__span">
+                        <svg class="professionalInfoBlock_summary__svg" id="svgEducation" width="14" height="12"
+                            viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.6666 3.33333V11.3333C13.6666 11.7015 13.3681 12 12.9999 12H0.999919C0.631732 12 0.333252 11.7015 0.333252 11.3333V2.66667H12.9999C13.3681 2.66667 13.6666 2.96515 13.6666 3.33333ZM7.27605 1.33333H0.333252V0.666667C0.333252 0.29848 0.631732 0 0.999919 0H5.94272L7.27605 1.33333Z" />
+                        </svg>
+                        Back-end
+                    </span>
+                </summary>
+            </details>
+            <details class="professionalInfoBlock_details">
+                <summary class="professionalInfoBlock_summary" id="backendButton">
+                    <span class="professionalInfoBlock_summary__span">
+                        <svg class="professionalInfoBlock_summary__svg" width="14" height="13"
+                            viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.6667 3.83333V11.8333C13.6667 12.2015 13.3682 12.5 13 12.5H0.999995C0.631808 12.5 0.333328 12.2015 0.333328 11.8333V3.16667H13C13.3682 3.16667 13.6667 3.46515 13.6667 3.83333ZM7.27613 1.83333H0.333328V1.16667C0.333328 0.79848 0.631808 0.5 0.999995 0.5H5.94279L7.27613 1.83333Z"
+                                fill="#FF637E" />
+                        </svg>
+                        Languages
+                    </span>
+                </summary>
+            </details>
+        </div>
+    </details>
+    @include('components.contacts-details-block')
+</div>
