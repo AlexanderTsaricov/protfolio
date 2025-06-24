eb48689dc6ed73dc518c65bd6c83438aa1454a39
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-06-24 19:02:44 +0500

    delete trash

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,10 +11,8 @@ class AuthServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Удалите или закомментируйте $this->registerPolicies();
 
         Gate::define('viewFilament', function (User $user): bool {
-            Log::info('Gate viewFilament проверка для пользователя ID ' . $user->id . ', is_admin=' . $user->is_admin);
             return (bool) $user->is_admin;
         });
     }
