40e5a1973bc9cbeb3173064e7bbdbd60f74d2786
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-06-24 19:42:20 +0500

    update professional-info.js

--- public/js/professional-info.js
+++ public/js/professional-info.js
@@ -4,18 +4,6 @@ import { setCloseEventToTabs } from "./info-functions.js";
 document.addEventListener("DOMContentLoaded", function () {
     const globalInfoNameBlock = "professionalInfoBlock";
 
-    const frontendButton = document.querySelector("#frontendButton");
-    const backendButton = document.querySelector("#backendButton");
-    const jsButton = document.querySelector("#javaScript");
-    const phpButton = document.querySelector("#php");
-    const javaButton = document.querySelector("#java");
-    const pythonButton = document.querySelector("#python");
-    const cSharpButton = document.querySelector("#cSharp");
-    const swiftButton = document.querySelector("#swift");
-    const rubyButton = document.querySelector("#ruby");
-    const sqlButton = document.querySelector("#sql");
-    const visualBasicButton = document.querySelector("#visualBasic");
-
     const state = {
         activeContentName: "",
         contentTabs: document.querySelector("#contentTabs"),
@@ -26,17 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
         contentBox: document.querySelector("#contentBox"),
     };
 
-    setCloseEventToTabs(state, globalInfoNameBlock);
+    const buttons = {
+        frontend: document.querySelector("#frontendButton"),
+        backend: document.querySelector("#backendButton"),
+        javaScript: document.querySelector("#javaScript"),
+        php: document.querySelector("#php"),
+        java: document.querySelector("#java"),
+        swift: document.querySelector("#swift"),
+        python: document.querySelector("#python"),
+        cSharp: document.querySelector("#cSharp"),
+        ruby: document.querySelector("#ruby"),
+        sql: document.querySelector("#sql"),
+        visualBasic: document.querySelector("#visualBasic"),
+    };
 
-    buttonEvent("frontend", frontendButton, state, globalInfoNameBlock);
-    buttonEvent("backend", backendButton, state, globalInfoNameBlock);
-    buttonEvent("javaScript", jsButton, state, globalInfoNameBlock);
-    buttonEvent("php", phpButton, state, globalInfoNameBlock);
-    buttonEvent("java", javaButton, state, globalInfoNameBlock);
-    buttonEvent("swift", swiftButton, state, globalInfoNameBlock);
-    buttonEvent("python", pythonButton, state, globalInfoNameBlock);
-    buttonEvent("cSharp", cSharpButton, state, globalInfoNameBlock);
-    buttonEvent("ruby", rubyButton, state, globalInfoNameBlock);
-    buttonEvent("sql", sqlButton, state, globalInfoNameBlock);
-    buttonEvent("visualBasic", visualBasicButton, state, globalInfoNameBlock);
+    setCloseEventToTabs(state, globalInfoNameBlock);
+    for (const key in buttons) {
+        if (buttons.hasOwnProperty(key)) {
+            buttonEvent(key, buttons[key], state, globalInfoNameBlock);
+        }
+    }
 });
