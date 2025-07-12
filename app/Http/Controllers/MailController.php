db0cd484d116f429be33c9f5331ad8aeec415fd5
AlexanderTsaricov <salispiligrim@yandex.ru>
2025-07-12 07:03:07 +0500

    update MailController - add security email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
+use App\Models\BlokedMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
+use Mews\Purifier\Facades\Purifier;
 
 class MailController extends Controller
 {
     public function sendMail(Request $request)
     {
         Log::info('sendMail() вызван', $request->all());
+        $email = Purifier::clean($request->input('email'));
+        if (BlokedMail::where('email', $email)->first()) {
+            return view('contact-me', ['bloked' => true]);
+        }
         $data = $request->validate([
             'name'    => 'required|string|max:255',
             'email'   => 'required|email|max:255',
